0413dfa584508d888cb6c2cfa57919d268ed5349
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-10-17 21:43:54 +0530

    fix: update package-lock.json and package.json for peer dependencies; refactor auth and config services for improved error handling

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,49 +2,54 @@ import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = new Client();
-    account;
+	client = new Client();
+	account;
 
-    constructor() {
-        this.client
-            .setEndpoint(conf.appwriteEndPoint)
-            .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client);
-    }
+	constructor() {
+		this.client
+			.setEndpoint(conf.appwriteEndPoint)
+			.setProject(conf.appwriteProjectId);
+		this.account = new Account(this.client);
+	}
 
-    async createAccount({ email, name, password }) {
-        try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name)
-            return userAccount ? this.login(email, password) : userAccount
-        } catch (error) {
-            console.log("Appwrite authentication service :: account creation error", error)
-        }
-    }
+	async createAccount({ email, name, password }) {
+		try {
+			const userAccount = await this.account.create(
+				ID.unique(),
+				email,
+				password,
+				name,
+			);
+			return userAccount ? this.login(email, password) : userAccount;
+		} catch (error) {
+			console.log("Appwrite authentication service :: account creation error", error);
+		}
+	}
 
-    async login({ email, password }) {
-        try {
-            return await this.account.createEmailPasswordSession(email, password)
-        } catch (error) {
-            console.log("Appwrite authentication service :: user login error")
-        }
-    }
+	async login({ email, password }) {
+		try {
+			return await this.account.createEmailPasswordSession(email, password);
+		} catch (error) {
+			console.log("Appwrite authentication service :: user login error", error);
+		}
+	}
 
-    async getCurrentUser() {
-        try {
-            return await this.account.get()
-        } catch (error) {
-            console.log("Appwrite authentication service :: fetching user account error")
-        }
-    }
+	async getCurrentUser() {
+		try {
+			return await this.account.get();
+		} catch (error) {
+			console.log("Appwrite authentication service :: fetching user account error", error);
+		}
+	}
 
-    async logout() {
-        try {
-            return await this.account.deleteSessions()
-        } catch (error) {
-            console.log("Appwrite authentication service :: user logout error")
-        }
-    }
+	async logout() {
+		try {
+			return await this.account.deleteSessions();
+		} catch (error) {
+			console.log("Appwrite authentication service :: user logout error", error);
+		}
+	}
 }
 
 const authService = new AuthService();
-export default authService
+export default authService;
